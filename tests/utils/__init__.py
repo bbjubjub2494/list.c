d28a7916c6494cd8496cb8cdd519aae535614664
Louis Bettens <louis@bettens.info>
2019-05-05 00:04:12 +0200

    add a doctest

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,8 +1,10 @@
-from .raise_signals import raise_signals, Signal
+from .raise_signals import FATAL_SIGNALS, raise_signals, Signal
 
 def load_tests(loader, standard_tests, pattern):
     from . import test_raise_signals
+    import doctest, importlib
     standard_tests.addTests((
         loader.loadTestsFromModule(test_raise_signals),
+        doctest.DocTestSuite(importlib.import_module('.raise_signals', package=__name__)),
     ))
     return standard_tests
