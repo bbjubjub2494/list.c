cbf98a2b8e39c015c706e6785307f20da7bf92e1
Louis Bettens <louis.bettens@epfl.ch>
2019-07-13 00:54:25 +0200

    test AttrProxy

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -2,10 +2,11 @@ from .raise_signals import FATAL_SIGNALS, raise_signals, Signal
 from .attr_proxy import AttrProxy
 
 def load_tests(loader, standard_tests, pattern):
-    from . import test_raise_signals
+    from . import test_raise_signals, test_attr_proxy
     import doctest, importlib
     standard_tests.addTests((
         loader.loadTestsFromModule(test_raise_signals),
+        loader.loadTestsFromModule(test_attr_proxy),
         doctest.DocTestSuite(importlib.import_module('.raise_signals', package=__name__)),
     ))
     return standard_tests
